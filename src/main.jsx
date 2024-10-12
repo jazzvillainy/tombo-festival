4e8acc9833249be316a053e44a4647062c1e3354
Quietonset <davidakachi9@gmail.com>
2024-10-12 23:57:00 +0100

    struggling

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'typeface-inter';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import '../src/index.css';
 
 
 createRoot(document.getElementById('root')).render(
