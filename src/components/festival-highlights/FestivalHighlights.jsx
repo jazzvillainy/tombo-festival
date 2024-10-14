1b63b50fecbbbed03417041e5d739b34b559433e
Quietonset <davidakachi9@gmail.com>
2024-10-14 16:35:43 +0100

    i am trying something new

---
+++ src/components/festival-highlights/FestivalHighlights.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import icons
+import { HiOutlineSparkles } from "react-icons/hi2";
+
+const FestivalHighlights = () => {
+  return (
+    <div className=''>
+      {/* texts */}
+      <div></div>
+      {/* images */}
+      <div></div>
+    </div>
+  )
+}
+
+export default FestivalHighlights;
