242cd1291c5dfeab185893a6e4d511bd774ba102
Quietonset <davidakachi9@gmail.com>
2024-10-15 11:16:08 +0100

    additional changes

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Highlights from '../../components/festival-highlights/Highlights';
+import TicketInfo from '../../components/ticket-informations/TicketInfo';
 
 
 const LandingPage = () => {
   return (
     <div className=''>
       <Highlights/>
+      <TicketInfo/>
     </div>
   )
 }
