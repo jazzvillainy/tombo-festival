f7bf8977d00e326bf57e85e686bd91251594442f
Quietonset <davidakachi9@gmail.com>
2024-10-15 12:18:44 +0100

    fixed a bug in navbar

--- src/components/nav-section/Navbar.jsx
+++ src/components/nav-section/Navbar.jsx
@@ -5,11 +5,7 @@ import logo from '../../assets/images/TomboLogo.png';
 
 const Navbar = () => {
   return (
-<<<<<<< HEAD
-    <nav className='flex justify-between px-24 pt-7 pb-2 bg-[#fcfcfc] sticky top-0'>
-=======
     <nav className='flex justify-between mx-[120px] py-5 bg-white sticky top-0 '>
->>>>>>> 77dd1e5b3e70ed7cb958d518241b89dfd6750f53
       {/* section links */}
       <div className='flex gap-x-12 justify-center items-center'>
         <Link to={'/'} className='hover:text-[#236e45]'>Home</Link>
